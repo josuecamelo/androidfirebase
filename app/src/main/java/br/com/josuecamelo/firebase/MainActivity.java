a401fe93bd786357c572ee6874b1146a8885b576
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2020-04-13 20:37:38 -0300

    Autenticando com Google

--- app/src/main/java/br/com/josuecamelo/firebase/MainActivity.java
+++ app/src/main/java/br/com/josuecamelo/firebase/MainActivity.java
@@ -1,21 +1,32 @@
 package br.com.josuecamelo.firebase;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.auth.AuthCredential;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
     private FirebaseAuth mFirebaseAuth;
+    private GoogleApiClient mGoogleApiClient;
     EditText editTextUser, editTextSenha;
 
     @Override
@@ -25,17 +36,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mFirebaseAuth = FirebaseAuth.getInstance();
 
+        /*editTextUser = (EditText) findViewById(R.id.editTextEmail);
+        editTextSenha = (EditText) findViewById(R.id.editTextSenha);
+        findViewById(R.id.btnLoginSenha).setOnClickListener(this);*/
+
         editTextUser = (EditText) findViewById(R.id.editTextEmail);
         editTextSenha = (EditText) findViewById(R.id.editTextSenha);
-        findViewById(R.id.btnLoginSenha).setOnClickListener(this);
+        findViewById(R.id.btnLoginGoogle).setOnClickListener(this);
+        findViewById(R.id.btnLogoutGoogle).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+            }
+        });
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
     }
 
     @Override
     public void onClick(View v) {
-        String user = editTextUser.getText().toString();
-        String senha = editTextSenha.getText().toString();
+        /*String user = editTextUser.getText().toString();
+        String senha = editTextSenha.getText().toString();*/
 
-        mFirebaseAuth.signInWithEmailAndPassword(user, senha)
+        /*mFirebaseAuth.signInWithEmailAndPassword(user, senha)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
@@ -46,6 +77,58 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                             Toast.makeText(MainActivity.this, task.getException().toString(), Toast.LENGTH_LONG).show();
                         }
                     }
+                });*/
+
+        signIn();
+    }
+
+    private void signIn(){
+        Intent intent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(intent, 1);
+    }
+
+    private void signOut(){
+        mFirebaseAuth.signOut();
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                Toast.makeText(MainActivity.this, "conta desconectada", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+
+            if (result.isSuccess()){
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseLogin(account);
+            }
+        }
+    }
+
+    private void firebaseLogin(GoogleSignInAccount account){
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        mFirebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            startActivity(new Intent(MainActivity.this, PrincipalActivity.class));
+                            finish();
+                        }else {
+                            Toast.makeText(MainActivity.this, "Falha na autenticação", Toast.LENGTH_SHORT).show();
+                        }
+                    }
                 });
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(MainActivity.this, "Falha na autenticação", Toast.LENGTH_SHORT).show();
     }
 }
