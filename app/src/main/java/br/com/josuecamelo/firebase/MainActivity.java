351e6173b3110ac78b3f7de97cdb1f4cd1d5655f
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2020-04-13 20:12:10 -0300

    Autenticando com

--- app/src/main/java/br/com/josuecamelo/firebase/MainActivity.java
+++ app/src/main/java/br/com/josuecamelo/firebase/MainActivity.java
@@ -1,14 +1,51 @@
 package br.com.josuecamelo.firebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private FirebaseAuth mFirebaseAuth;
+    EditText editTextUser, editTextSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        editTextUser = (EditText) findViewById(R.id.editTextEmail);
+        editTextSenha = (EditText) findViewById(R.id.editTextSenha);
+        findViewById(R.id.btnLoginSenha).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String user = editTextUser.getText().toString();
+        String senha = editTextSenha.getText().toString();
+
+        mFirebaseAuth.signInWithEmailAndPassword(user, senha)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            startActivity(new Intent(MainActivity.this, PrincipalActivity.class));
+                            finish();
+                        }else {
+                            Toast.makeText(MainActivity.this, task.getException().toString(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 }
