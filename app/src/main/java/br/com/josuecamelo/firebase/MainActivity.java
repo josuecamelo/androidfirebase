271b27a2f6a383674ed17b9319357bb8721c04a7
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2020-04-13 21:20:10 -0300

    Autenticação com Facebook

--- app/src/main/java/br/com/josuecamelo/firebase/MainActivity.java
+++ app/src/main/java/br/com/josuecamelo/firebase/MainActivity.java
@@ -9,6 +9,12 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -20,14 +26,16 @@ import com.google.android.gms.common.api.Status;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.auth.AuthCredential;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
+public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mFirebaseAuth;
-    private GoogleApiClient mGoogleApiClient;
     EditText editTextUser, editTextSenha;
+
+    private CallbackManager mCallbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,83 +44,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mFirebaseAuth = FirebaseAuth.getInstance();
 
-        /*editTextUser = (EditText) findViewById(R.id.editTextEmail);
-        editTextSenha = (EditText) findViewById(R.id.editTextSenha);
-        findViewById(R.id.btnLoginSenha).setOnClickListener(this);*/
-
         editTextUser = (EditText) findViewById(R.id.editTextEmail);
         editTextSenha = (EditText) findViewById(R.id.editTextSenha);
-        findViewById(R.id.btnLoginGoogle).setOnClickListener(this);
-        findViewById(R.id.btnLogoutGoogle).setOnClickListener(new View.OnClickListener() {
+
+        mCallbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = (LoginButton) findViewById(R.id.button_facebook_login);
+        loginButton.setReadPermissions("email","public_profile");
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
             @Override
-            public void onClick(View v) {
-                signOut();
+            public void onSuccess(LoginResult loginResult) {
+                firebaseLogin(loginResult.getAccessToken());
             }
-        });
 
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this, this)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build();
-    }
-
-    @Override
-    public void onClick(View v) {
-        /*String user = editTextUser.getText().toString();
-        String senha = editTextSenha.getText().toString();*/
-
-        /*mFirebaseAuth.signInWithEmailAndPassword(user, senha)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()){
-                            startActivity(new Intent(MainActivity.this, PrincipalActivity.class));
-                            finish();
-                        }else {
-                            Toast.makeText(MainActivity.this, task.getException().toString(), Toast.LENGTH_LONG).show();
-                        }
-                    }
-                });*/
-
-        signIn();
-    }
-
-    private void signIn(){
-        Intent intent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-        startActivityForResult(intent, 1);
-    }
-
-    private void signOut(){
-        mFirebaseAuth.signOut();
-        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(new ResultCallback<Status>() {
             @Override
-            public void onResult(@NonNull Status status) {
-                Toast.makeText(MainActivity.this, "conta desconectada", Toast.LENGTH_SHORT).show();
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
             }
         });
     }
+
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == 1){
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-
-            if (result.isSuccess()){
-                GoogleSignInAccount account = result.getSignInAccount();
-                firebaseLogin(account);
-            }
-        }
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
-    private void firebaseLogin(GoogleSignInAccount account){
-        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+    private void firebaseLogin(AccessToken token){
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
         mFirebaseAuth.signInWithCredential(credential)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
@@ -127,8 +93,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 });
     }
 
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        Toast.makeText(MainActivity.this, "Falha na autenticação", Toast.LENGTH_SHORT).show();
-    }
 }
+
